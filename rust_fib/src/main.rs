91c924e4f60a942ddb869f36e72d653ff3a79eb8
Uwe Kubosch <uwe@kubosch.no>
2016-05-17 20:02:14 +0200

    Updated README.md with purpose and measurements Added source and build instructions

--- rust_fib/src/main.rs
+++ rust_fib/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: fib <n>");
+        return;
+    }
+    let n = args[1].parse::<i32>().unwrap();
+    println!("fib({}) = {}", n, fib(n));
+}
+
+fn fib(n: i32) -> i64 {
+    if n <= 2 {
+        return 1;
+    }
+    return fib(n - 2) + fib(n - 1);
 }
